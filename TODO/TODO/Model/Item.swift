80e9b1e9b929a082e981776c835b8ea7c17e823b
Rumit Man Tandukar <rumit0270@gmail.com>
2019-09-14 11:51:57 +0545

    remove the coredata models and add Realm object models

--- TODO/TODO/Model/Item.swift
+++ TODO/TODO/Model/Item.swift
@@ -7,8 +7,19 @@
 //
 
 import Foundation
+import RealmSwift
 
 //class Item: Codable {
 //    var title: String = ""
 //    var done: Bool = false
 //}
+
+// Object is a Real class to define the Realm model objects
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool =  false
+    
+    // define the inverse relationship for Item
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+}
+
